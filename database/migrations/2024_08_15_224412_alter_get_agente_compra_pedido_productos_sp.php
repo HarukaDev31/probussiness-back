827edaa72ce671897b6c74801109844068bbe004
HarukaDev31 <harukakasugano31@gmail.com>
2024-08-15 18:03:05 -0500

    fix

--- database/migrations/2024_08_15_224412_alter_get_agente_compra_pedido_productos_sp.php
+++ database/migrations/2024_08_15_224412_alter_get_agente_compra_pedido_productos_sp.php
@@ -15,7 +15,8 @@ class AlterGetAgenteCompraPedidoProductosSp extends Migration
         $sp = "CREATE PROCEDURE get_agente_compra_pedido_productos(in p_id_producto int)
         begin
 	select
-	*
+	*,
+    (select cotizacionCode from agente_compra_pedido_cabecera where ID_Pedido_Cabecera=p_id_producto) as cotizacionCode
 	from agente_compra_pedido_detalle acpd
 	join agente_compra_pedido_detalle_producto_proveedor acpdpp on acpdpp.ID_Pedido_Cabecera=p_id_producto
 	and acpd.ID_Pedido_Detalle=acpdpp.ID_Pedido_Detalle
